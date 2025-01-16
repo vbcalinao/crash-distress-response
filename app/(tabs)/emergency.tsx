07c91acf78fdc1bfb6209be07510b1c760162df2
Victor Calinao Jr <vbcalinao@gmail.com>
2025-01-17 01:17:01 +0800

    change icon to plus app emergency tab

--- app/(tabs)/emergency.tsx
+++ app/(tabs)/emergency.tsx
@@ -69,7 +69,7 @@ export default function TabTwoScreen() {
                     <IconSymbol
                         size={310}
                         color="#808080"
-                        name="chevron.left.forwardslash.chevron.right"
+                        name="plus.app"
                         style={styles.headerImage}
                     />
                 }
